1771fb2a01e254e2616a71ccd01d7c25b5e08d4a
liuyunzhi <Lyz19960503>
2022-06-25 23:01:38 +0800

    feat: 配置动态 rem

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './style/index.scss'
 import router from './router'
+import { useREM } from './utils/flexible'
 
+useREM()
 createApp(App).use(router).mount('#app')
