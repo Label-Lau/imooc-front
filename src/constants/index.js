7e0795da6d350e8794308148d832e58f64ef8273
liuyunzhi <Lyz19960503>
2022-06-27 22:30:34 +0800

    feat: 补全 category 数据

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,3 +1,8 @@
+export const ICODE = 'C0FD7308BEA57817'
 // PC 设备指定宽度
 export const PC_DEVICE_WIDTH = 1280
-export const ICODE = 'C0FD7308BEA57817'
+// category 的本地构建数据
+export const ALL_CATEGORY_ITEM = {
+  id: 'all',
+  name: '全部'
+}
--- src/views/main/components/navigation/mobile/index.vue
+++ src/views/main/components/navigation/mobile/index.vue
@@ -2,7 +2,14 @@
   <div class="bg-white sticky top-0 left-0 z-10">
     <ul
       class="relative flex overflow-x-auto p-1 text-xs text-zinc-600 overflow-hidden"
+      ref="ulTarget"
     >
+      <!-- 滑块 -->
+      <li
+        ref="sliderTarget"
+        class="absolute h-[22px] bg-zinc-900 rounded-lg duration-200"
+        :style="sliderStyle"
+      ></li>
       <!-- 汉堡按钮 -->
       <li
         class="z-20 fixed top-0 right-[-1px] h-4 px-1 flex items-center bg-white shadow-l-white"
@@ -12,9 +19,12 @@
 
       <!-- category item -->
       <li
-        v-for="item in data"
+        v-for="(item, index) in data"
         :key="item.id"
         class="shrink-0 px-1.5 py-0.5 z-10 duration-200 last:mr-4"
+        :class="{ 'text-zinc-100': currentCategoryIndex === index }"
+        :ref="setItemRef"
+        @click="onItemClick(index)"
       >
         {{ item.name }}
       </li>
@@ -22,10 +32,49 @@
   </div>
 </template>
 <script setup>
+import { ref, watch, onBeforeUpdate } from 'vue'
+import { useScroll } from '@vueuse/core'
+
 defineProps({
   data: {
     type: Array,
     required: true
   }
 })
+
+// 滑块
+const sliderStyle = ref({
+  transform: 'translateX(0px)',
+  width: '52px'
+})
+// 选中 item 的下标
+const currentCategoryIndex = ref(0)
+// 获取填充的 item 元素
+let itemRefs = []
+const setItemRef = (el) => {
+  if (el) {
+    itemRefs.push(el)
+  }
+}
+onBeforeUpdate(() => {
+  itemRefs = []
+})
+// 获取 ul 元素，以计算偏移位置
+const ulTarget = ref(null)
+const { x: ulScrollLeft } = useScroll(ulTarget)
+watch(currentCategoryIndex, (val) => {
+  // 获取选中元素的 left、width
+  const { left, width } = itemRefs[val].getBoundingClientRect()
+  // 为 sliderStyle 设置属性
+  const ulPadding = parseFloat(getComputedStyle(ulTarget.value)['padding'])
+  sliderStyle.value = {
+    // ul 横向滚动位置 + 当前元素的 left 偏移量
+    transform: `translateX(${ulScrollLeft.value + left - ulPadding}px)`,
+    width: width + 'px'
+  }
+})
+// item 点击事件
+const onItemClick = (index) => {
+  currentCategoryIndex.value = index
+}
 </script>
