4c244fcdd720b7a52b0b33c0c139d5c9baa2d93c
liuyunzhi <Lyz19960503>
2022-07-06 09:12:12 +0800

    feat: 完善 search 基本能力

--- src/views/layout/components/header/components/header-search/index.vue
+++ src/views/layout/components/header/components/header-search/index.vue
@@ -1,7 +1,17 @@
 <template>
-  <div>search</div>
+  <div class="w-full">
+    <m-search v-model="inputValue">
+      <template #dropdown>
+        <div>dropdown</div>
+      </template>
+    </m-search>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+const inputValue = ref('')
+</script>
 
 <style lang="scss" scoped></style>
