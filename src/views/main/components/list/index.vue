21f7d898ce0f16c9bcda5cda86ad8acb984359f7
liuyunzhi <Lyz19960503>
2022-07-12 23:13:35 +0800

    feat: 完成瀑布流触底加载

--- src/views/main/components/list/index.vue
+++ src/views/main/components/list/index.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
-    <m-waterfall
-      :data="pexelsList"
-      :column="isMobileTerminal ? 2 : 5"
-      :picturePreReading="false"
-      class="w-full px-1"
+    <!-- 列表处理 -->
+    <m-infinite-list
+      v-model="isLoading"
+      :isFinished="isFinished"
+      @onLoad="getPexelsData"
     >
-      <template v-slot="{ item, width }">
-        <itemVue :data="item" :width="width" />
-      </template>
-    </m-waterfall>
+      <m-waterfall
+        :data="pexelsList"
+        :column="isMobileTerminal ? 2 : 5"
+        :picturePreReading="false"
+        class="w-full px-1"
+      >
+        <template v-slot="{ item, width }">
+          <itemVue :data="item" :width="width" />
+        </template>
+      </m-waterfall>
+    </m-infinite-list>
   </div>
 </template>
 
@@ -17,6 +24,7 @@
 import { ref } from 'vue'
 import { getPexelsList } from '@/api/pexels'
 import itemVue from './item.vue'
+import mInfiniteList from '@/libs/infinite-list/index.vue'
 import { isMobileTerminal } from '@/utils/flexible'
 
 /**
@@ -26,12 +34,38 @@ let query = {
   page: 1,
   size: 20
 }
+// 数据是否在加载中
+const isLoading = ref(false)
+// 数据是否全部加载完成
+const isFinished = ref(false)
+// 数据源
 const pexelsList = ref([])
+/**
+ * 加载数据的方法
+ */
 const getPexelsData = async () => {
+  // 数据全部加载完成则 return
+  if (isFinished.value) {
+    return
+  }
+  // 完成第一次请求之后，后续请求让 page 自增
+  if (pexelsList.value.length) {
+    query.page += 1
+  }
+  // 触发接口请求
   const res = await getPexelsList(query)
-  pexelsList.value = res.list
+  if (query.page === 1) {
+    pexelsList.value = res.list
+  } else {
+    pexelsList.value.push(...res.list)
+  }
+  // 判断数据是否全部加载完成
+  if (pexelsList.value.length === res.total) {
+    isFinished.value = true
+  }
+  // 修改 loading 标记
+  isLoading.value = false
 }
-getPexelsData()
 </script>
 
 <style lang="scss" scoped></style>
