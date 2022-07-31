a0e925741c4064628b160b00179eb3d8763b2a8b
liuyunzhi <Lyz19960503>
2022-07-31 22:48:27 +0800

    feat: 完成兔小巢反馈功能

--- src/constants/index.js
+++ src/constants/index.js
@@ -46,5 +46,7 @@ export const LOGIN_TYPE_WX = 'WX'
 // STS 上传数据
 export const REGION = 'oss-cn-beijing'
 export const BUCKET = 'imooc-front'
+// 兔小巢反馈地址
+export const FEEDBACK_URL = 'https://support.qq.com/product/383681'
 
 
